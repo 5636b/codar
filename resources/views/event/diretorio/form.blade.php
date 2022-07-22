620ed20a2e4873111ba7b92774188e340d646c70
5636b <mush.comushroomhead@hotmail.com>
2022-07-22 20:26:45 -0300

    atualização de projeto

--- resources/views/event/diretorio/form.blade.php
+++
@@ -1,8 +0,0 @@
-@csrf
-<input type="text" name="title" id="title" placeholder="Título">
-<input type="text" name="description" id="description" placeholder="Descrição">
-<input type="text" name="city" id="city" placeholder="Cidade">
-<input type="text" name="private" id="private" placeholder="Privado">
-<button type="submit">
-    Enviar
-</button>
